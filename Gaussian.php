306d28b9f3f71304b14b850aad583b9031cf6a9a
Vicktor Desrony <if319028@students.del.ac.id>
2021-02-24 02:51:48 +0700

    Progress02

---
+++ Gaussian.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/css/bootstrap.css">
+    <link rel="stylesheet" href="Assets/css/myStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+    Set Ukuran Matriks : <input type="text" maxSize="5" size="5" class="inp inp-sz5"> <input type="submit" class="btn btn-info" value="Start"> 
+    </div>
+</body>
+<script src="Assets/js/bootstrap.bundle.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</html>
